ed7804f170c1dab734aba5bd0dc1f7ccaa934f0a
vilas-nevkar <vilas.nevkar@gmail.com>
2024-08-04 17:59:59 +0530

    update search functionality

---
+++ search.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+def search_d_process_text_files(directory):
+    """
+
+    :param directory:
+    :return:
+    """
+    pass
+
+
+def search_d_process_txt_files(directory):
+    """
+
+    :param directory:
+    :return:
+    """
+    pass
